36f95db7842f9dbb5cedabfb8dbac9d2bcaf9e25
Sirpak <daveczerepak@gmail.com>
2025-08-18 02:21:02 -0400

    Stretch covemind2 image horizontally to fit words better in navigation bar

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -22,7 +22,7 @@ export default function Navigation() {
               <img 
                 src="/covemind2.png" 
                 alt="Covemind" 
-                className="h-8 w-auto"
+                className="h-8 w-32 object-contain"
               />
             </div>
           </div>
